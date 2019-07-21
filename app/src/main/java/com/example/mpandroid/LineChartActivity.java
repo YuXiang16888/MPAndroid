f9c89ba5925dcad13bc1f81a574ff2f16c1dc72c
Yushiang Wang <40443106@gm.nfu.edu.tw>
2019-07-21 19:19:55 +0800

    1.add BarChart And PieChart(Not Yet)

--- app/src/main/java/com/example/mpandroid/LineChartActivity.java
+++ app/src/main/java/com/example/mpandroid/LineChartActivity.java
@@ -4,8 +4,11 @@ import android.graphics.Color;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -17,8 +20,8 @@ import java.util.List;
 public class LineChartActivity extends AppCompatActivity {
 
     private LineChart mLineChart;
-    private LineData mLineData;
-    private LineDataSet mLineDataSet;
+    private BarChart mBarChart;
+    private PieChart mPieChart;
     private Runnable mRunnable;
     private Handler mHandler;
     private int i = 0;
@@ -27,13 +30,21 @@ public class LineChartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_line_chart);
         mLineChart = findViewById(R.id.mp_chart_line);
-        MpChart.lineMpChart(mLineChart,true,0,0);
+        mBarChart = findViewById(R.id.mp_chart_bar);
+        mPieChart = findViewById(R.id.mp_chart_pie);
+        mLineChart.setVisibility(View.GONE);
+        mBarChart.setVisibility(View.GONE);
+        //MpChart.lineMpChart(mLineChart,0,0);
+        //MpChart.barMpChart(mBarChart,0,0);
+        MpChart.pieMpChart(mPieChart,"",0);
         initRunnable();
     }
 
     private void addEntryPoint() {
-        float random = (float)(Math.random() * 10);
-        MpChart.lineMpChart(mLineChart,false,i,random);
+        float random = (float)(Math.random() * 10 + 2);
+        //MpChart.lineMpChart(mLineChart,i,random);
+        //MpChart.barMpChart(mBarChart,i,random);
+        MpChart.pieMpChart(mPieChart,"Part" + i,random);
         i++;
     }
 
@@ -43,7 +54,7 @@ public class LineChartActivity extends AppCompatActivity {
             @Override
             public void run() {
                 addEntryPoint();
-                if(i < 100)
+                if(i < 10)
                     mHandler.postDelayed(mRunnable,100);
             }
         };
--- app/src/main/java/com/example/mpandroid/MpChart.java
+++ app/src/main/java/com/example/mpandroid/MpChart.java
@@ -4,23 +4,33 @@ import android.graphics.Color;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.DataSet;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.IPieDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MpChart {
 
     //LineMPChart
 
-    public static void lineMpChart(LineChart lineChart,boolean initFlag,float time,float value){
+    public static void lineMpChart(LineChart lineChart, float time, float value) {
 
-        if(lineChart.getData() == null){
-
+        if (lineChart.getData() == null) {
             LineData lineData = new LineData();
             lineChart.setData(lineData);
 
@@ -54,21 +64,18 @@ public class MpChart {
             yAxis.setAxisMaximum(15);
 
 
-        }else {
-
+        } else {
             LineData lineData = lineChart.getLineData();
-
             ILineDataSet iLineDataSet = lineData.getDataSetByIndex(0);
-            if(iLineDataSet == null){
-
+            if (iLineDataSet == null) {
                 iLineDataSet = createLineSet();
                 lineData.addDataSet(iLineDataSet);
             }
-            if(iLineDataSet.getEntryCount() > 50){
+            if (iLineDataSet.getEntryCount() > 50) {
                 iLineDataSet.removeFirst();
             }
 
-            iLineDataSet.addEntry(new Entry(time,value));
+            iLineDataSet.addEntry(new Entry(time, value));
             lineData.notifyDataChanged();
             lineChart.notifyDataSetChanged();
             lineChart.moveViewToX(time);
@@ -76,82 +83,94 @@ public class MpChart {
 
     }
 
-    private static LineDataSet createLineSet(){
+    private static LineDataSet createLineSet() {
 
-        LineDataSet lineDataSet = new LineDataSet(null,"LineChart");
+        LineDataSet lineDataSet = new LineDataSet(null, "LineChart");
         lineDataSet.setHighlightEnabled(true);
         lineDataSet.setHighLightColor(Color.parseColor("#FF0000"));
         return lineDataSet;
     }
 
 
-
     //BarMPChart
 
-    public static void barMpChart(BarChart barChart,int time,float value){
+    public static void barMpChart(BarChart barChart, int time, float value) {
 
         BarData barData = barChart.getData();
-        if(barData == null){
-
+        if (barData == null) {
             barData = new BarData();
             barChart.setData(barData);
 
+            barChart.setTouchEnabled(true);
+
+            XAxis xAxis = barChart.getXAxis();
+            xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+            xAxis.setLabelCount(12);
+            xAxis.setGranularity(2);
+            xAxis.setTextColor(Color.RED);
+            xAxis.setGridColor(Color.RED);
+            xAxis.setEnabled(true);
+
+            YAxis yAxis = barChart.getAxisLeft();
+            yAxis.setTextColor(Color.RED);
+            yAxis.setGridColor(Color.RED);
+            yAxis.setLabelCount(10);
+            yAxis.setAxisMinimum(0);
+            yAxis.setAxisMaximum(15);
 
         } else {
-
-
+            IBarDataSet iBarDataSet = barData.getDataSetByIndex(0);
+            if (iBarDataSet == null) {
+                iBarDataSet = createBarSet();
+                barData.addDataSet(iBarDataSet);
+            }
+            iBarDataSet.addEntry(new BarEntry(time, value));
+            barData.notifyDataChanged();
+            barChart.notifyDataSetChanged();
+            barChart.moveViewToX(time);
         }
     }
 
+    private static BarDataSet createBarSet() {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        List<BarEntry> barEntryList = new ArrayList<>();
+        BarDataSet barDataSet = new BarDataSet(barEntryList, "BarChart");
+        barDataSet.setHighlightEnabled(true);
+        barDataSet.setHighLightColor(Color.parseColor("#FF0000"));
+        return barDataSet;
+    }
 
 
     //PieMPChart
 
+    public static void pieMpChart(PieChart pieChart, String name, float value) {
+
+        PieData pieData = pieChart.getData();
+        IPieDataSet iPieDataSet;
+        if (pieData == null) {
+            pieData = new PieData();
+            iPieDataSet = createPieSet();
+            pieData.setDataSet(iPieDataSet);
+            pieChart.setData(pieData);
+
+            pieChart.setUsePercentValues(true);
+            pieChart.setExtraOffsets(5, 10, 5, 5);
+            pieChart.setHoleRadius(58f);
+            pieChart.setTransparentCircleRadius(61f);
+        } else {
+            iPieDataSet = pieData.getDataSetByIndex(0);
+            iPieDataSet.addEntry(new PieEntry(value, name));
+            pieData.notifyDataChanged();
+            pieChart.notifyDataSetChanged();
+        }
+    }
 
 
+    private static PieDataSet createPieSet() {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //
-
-
-
+        PieDataSet pieDataSet = new PieDataSet(null, "PieChart");
+        return pieDataSet;
+    }
 
 
 }
